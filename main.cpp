32916618112e2fda01126f3b018c83f4346b076d
Ch1eti <146098402+Ch1eti@users.noreply.github.com>
2023-09-26 21:20:40 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+   int age = 20;
+   double price = 66.6;
+   return 0;
+}
+
+
+
+// 计算机能理解二进制，只有1和0，存储一个1就是一个bit比特位，sizeof(long) >= sizeof(int)
+// 创建变量的本质是想内存申请空间
+// char -- 字符数据类型
+// short -- 短整型
+// int -- 整型
+// long -- 长整型
+// long long -- 更长的整型
+// float -- 单精度浮点数
+// double -- 双精度浮点数
+// 计算机中的单位：bit - 比特位；8；  byt - 字节  1024  kb 1024  mb   gb   tb  pb
+
